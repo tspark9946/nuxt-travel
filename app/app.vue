c9c62789f33592df9b8a12f8213f0462d87f5dd0
Park taeseong <tspark9946@gmail.com>
2025-10-21 19:26:35 +0900

    feat: ui library install

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
+  <NuxtRouteAnnouncer />
+
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl text-blue-500 font-bold underline">Hello world!</h1>
+    <button class="btn btn-secondary">View Map <Icon name="uil:github" /></button>
   </div>
 </template>
