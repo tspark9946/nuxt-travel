b52b6c005193bf0cb5c7fa0d25859dc633594d1d
Park taeseong <tspark9946@gmail.com>
2025-10-22 19:31:15 +0900

    feat: ui first

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,5 @@
 <template>
-  <NuxtRouteAnnouncer />
-
-  <div>
-    <h1 class="text-3xl text-blue-500 font-bold underline">Hello world!</h1>
-    <button class="btn btn-secondary">View Map <Icon name="uil:github" /></button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">Travel Log</NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn">Sign In</a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
